d0af8b2636c79114cfe3333a24b1a46cae08b5d8
choijh2da <korean011216@naver.com>
2025-06-03 16:33:42 +0900

    [추가] SHA256 + SALT로 패스워드 암호화 및 로그인 기능 구현.

--- src/main/java/PL_25/shuttleplay/Controller/LoginController.java
+++ src/main/java/PL_25/shuttleplay/Controller/LoginController.java
@@ -4,8 +4,8 @@ import PL_25.shuttleplay.Entity.User.NormalUser;
 import PL_25.shuttleplay.Service.LoginService;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -33,18 +33,23 @@ public class LoginController {
 
         Map<String, Object> response = new HashMap<>();
 
-        System.out.println("받기 성공");
         try {
 
+            // 해당 유저가 유효한 유저인지 확인.
             NormalUser loginUser = loginService.checkIsValidUser(id, password);
-            if (loginUser != null) {
-                httpSession.setAttribute("loginUser", loginUser.getUserId());
-                System.out.println("로그인 성공!!");
-            }
-            // 세션 추가 코드...
-            return null;
+
+            // 해당 유저 pk로 session 설정, 세션 관련 설정은 CustomSessionListener 참고.
+            httpSession.setAttribute("loginUser", loginUser.getUserId());
+
+            response.put("status", 200);
+            response.put("message", "로그인에 성공했습니다.");
+            return ResponseEntity.status(HttpStatus.OK).body(response);
+
         } catch (Exception e) {
-            return null;
+
+            response.put("status", 400);
+            response.put("error", "로그인에 실패했습니다.");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
         }
     }
 
